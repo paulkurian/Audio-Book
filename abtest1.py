59051f99b8275b50fd39155d4a17db9032ee9fb3
paulkurian <paul.kurian_ug19@ashoka.edu.in>
2017-05-03 22:06:15 +0530

    Variable names
    Made the variable names clearer

--- abtest1.py
+++ abtest1.py
@@ -2,21 +2,34 @@ import pyttsx
 from Tkinter import *
 engine = pyttsx.init()
 
-def audiobook(E1,E2,E3,E4):
-    rate = engine.getProperty('rate')
-    e1=int(E1.get())
-    e2=int(E2.get())
-    e3=E3.get()
-    e4=E4.get()
+def audiobook(text_name,rate_var,voice_var,censor):
     
-    engine.setProperty('rate', rate+e1)
+    Rate_var=int(rate_var.get())
+    
+    Voice_var=voice_var.get()
+    Text_name=text_name.get()
+    
+    Censor=censor.get()
+    
+
+        
+    
+    
+    if Voice_var=='M' or 'm':
+        intvoice=0
+    elif Voice_var=='F' or 'f':
+        intvoice=1
+    else:
+        print "Invalid Voice type entered"
+    rate = engine.getProperty('rate')
+    engine.setProperty('rate', rate+Rate_var)
     voices = engine.getProperty('voices')
-    engine.setProperty('voice', voices[e2].id)
-    text_file = open(e3, "r")
-    lines = text_file.readlines()
+    engine.setProperty('voice', voices[intvoice].id)
+    open_text = open(Text_name, "r")
+    lines = open_text.readlines()
     lines1=''
     
-    lis=e4.split()
+    lis=Censor.split()
     l=len(lis)
     for i in range (l):
         lis[i]=lis[i].lower()
@@ -51,27 +64,29 @@ def audiobook(E1,E2,E3,E4):
     
 root = Tk()
 
-label1 = Label( root, text="Rate")
-E1 = Entry(root, bd =5)
+label1 = Label( root, text="Rate (-100 to 100, 0 for normal speed) **")
+rate_var = Entry(root, bd =5)
 
-label2 = Label( root, text="Voice type")
-E2 = Entry(root, bd =5)
+label2 = Label( root, text="Voice type (Enter 'M' for Male and 'F' for Female)")
+voice_var = Entry(root, bd =5)
 
-label3 = Label( root, text="Text file name")
-E3 = Entry(root, bd =5)
+label3 = Label( root, text="Text file name Eg:Pride_and_Prejudice.txt **")
+text_name = Entry(root, bd =5)
 
-label4 = Label( root, text="Words to be censored")
-E4 = Entry(root, bd =5)
+label4 = Label( root, text="Words to be censored: (Enter the words you want censored out and separate each with a space)")
+censor = Entry(root, bd =5)
 
-submit = Button(root, text ="Submit", command = lambda: audiobook(E1,E2,E3,E4))
+label5 = Label( root, text="** Mandatory fields")
+submit = Button(root, text ="Submit", command = lambda: audiobook(text_name,rate_var,voice_var,censor))
 
 label1.pack()
-E1.pack()
+rate_var.pack()
 label2.pack()
-E2.pack()
+voice_var.pack()
 label3.pack()
-E3.pack()
+text_name.pack()
 label4.pack()
-E4.pack()
+censor.pack()
+label5.pack()
 submit.pack(side =BOTTOM) 
 root.mainloop()
